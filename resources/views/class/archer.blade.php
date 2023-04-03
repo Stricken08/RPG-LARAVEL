a0a56912fd5a1c7f60d353aa6db1e9134c5e218f
Aamndine <amandine.l@live.fr>
2023-04-03 15:29:26 +0200

    arc

--- resources/views/class/archer.blade.php
+++ resources/views/class/archer.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.layout')
 @section('content')
-<div class="class"><img class="class" src="/images/archer.jpeg" alt="archer" /></div>
+<div class="class"><img class="class" src="/images/archer3.jpg" alt="archer" /></div>
 <h2 class="epicTitle">L'archer</h2>
 <p class="parag">Les Archers combattent leurs ennemis à bonne distance, ordonnant à leurs fidèles familiers d’attaquer tandis qu’ils font pleuvoir les flèches ou les balles, ou empoignent leurs armes d’hast. Si leurs attaques à distance sont dévastatrices à longue comme à courte portée, les chasseurs sont aussi extrêmement mobiles. Ils peuvent esquiver ou ralentir leurs adversaires et ainsi garder le contrôle sur le champ de bataille.
 </p>
